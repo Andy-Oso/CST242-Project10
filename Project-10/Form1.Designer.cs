8fa80442af8936d8b4dfdec1cf483e6305a2d9cd
Andy <47667979+Andy-Oso@users.noreply.github.com>
2019-05-03 23:42:35 -0400

    Added Functionality + Comments/Documentation

--- Project-10/Form1.Designer.cs
+++ Project-10/Form1.Designer.cs
@@ -29,19 +29,26 @@
         private void InitializeComponent()
         {
             this.lblTitle = new System.Windows.Forms.Label();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.checkBoxCalculateOnlySubTotal = new System.Windows.Forms.CheckBox();
-            this.lblTaxPercent = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.lblSubTotal = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.groupBoxTipPercent = new System.Windows.Forms.GroupBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.tipPanel = new System.Windows.Forms.Panel();
             this.groupBoxPersonCount = new System.Windows.Forms.GroupBox();
             this.comboBoxSplitCount = new System.Windows.Forms.ComboBox();
-            this.panel1.SuspendLayout();
-            this.groupBoxTipPercent.SuspendLayout();
+            this.groupBoxTipPercent = new System.Windows.Forms.GroupBox();
+            this.comboBoxTipPercent = new System.Windows.Forms.ComboBox();
+            this.checkBoxCalculateOnlySubTotal = new System.Windows.Forms.CheckBox();
+            this.lblTaxPercent = new System.Windows.Forms.Label();
+            this.textBoxSubTotalInput = new System.Windows.Forms.TextBox();
+            this.lblSubTotal = new System.Windows.Forms.Label();
+            this.textBoxSalesTaxInput = new System.Windows.Forms.TextBox();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.lblTipTotal = new System.Windows.Forms.Label();
+            this.lblSalesTaxTotal = new System.Windows.Forms.Label();
+            this.textBoxTipOutput = new System.Windows.Forms.TextBox();
+            this.textBoxSalesTaxOutput = new System.Windows.Forms.TextBox();
+            this.dollarLabel = new System.Windows.Forms.Label();
+            this.dollarLabelTax = new System.Windows.Forms.Label();
+            this.tipPanel.SuspendLayout();
             this.groupBoxPersonCount.SuspendLayout();
+            this.groupBoxTipPercent.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblTitle
@@ -54,28 +61,77 @@
             this.lblTitle.TabIndex = 0;
             this.lblTitle.Text = "Tip Calculator";
             // 
-            // panel1
+            // tipPanel
             // 
-            this.panel1.Controls.Add(this.groupBoxPersonCount);
-            this.panel1.Controls.Add(this.groupBoxTipPercent);
-            this.panel1.Controls.Add(this.checkBoxCalculateOnlySubTotal);
-            this.panel1.Controls.Add(this.lblTaxPercent);
-            this.panel1.Controls.Add(this.textBox1);
-            this.panel1.Controls.Add(this.lblSubTotal);
-            this.panel1.Controls.Add(this.textBox2);
-            this.panel1.Location = new System.Drawing.Point(18, 43);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(354, 111);
-            this.panel1.TabIndex = 1;
+            this.tipPanel.Controls.Add(this.groupBoxPersonCount);
+            this.tipPanel.Controls.Add(this.groupBoxTipPercent);
+            this.tipPanel.Controls.Add(this.checkBoxCalculateOnlySubTotal);
+            this.tipPanel.Controls.Add(this.lblTaxPercent);
+            this.tipPanel.Controls.Add(this.textBoxSubTotalInput);
+            this.tipPanel.Controls.Add(this.lblSubTotal);
+            this.tipPanel.Controls.Add(this.textBoxSalesTaxInput);
+            this.tipPanel.Location = new System.Drawing.Point(18, 43);
+            this.tipPanel.Name = "tipPanel";
+            this.tipPanel.Size = new System.Drawing.Size(354, 111);
+            this.tipPanel.TabIndex = 1;
+            // 
+            // groupBoxPersonCount
+            // 
+            this.groupBoxPersonCount.Controls.Add(this.comboBoxSplitCount);
+            this.groupBoxPersonCount.Location = new System.Drawing.Point(234, 9);
+            this.groupBoxPersonCount.Name = "groupBoxPersonCount";
+            this.groupBoxPersonCount.Size = new System.Drawing.Size(100, 45);
+            this.groupBoxPersonCount.TabIndex = 2;
+            this.groupBoxPersonCount.TabStop = false;
+            this.groupBoxPersonCount.Text = "Tip Split";
+            // 
+            // comboBoxSplitCount
+            // 
+            this.comboBoxSplitCount.FormattingEnabled = true;
+            this.comboBoxSplitCount.Items.AddRange(new object[] {
+            "1.0",
+            "2.0",
+            "3.0",
+            "4.0",
+            "5.0"});
+            this.comboBoxSplitCount.Location = new System.Drawing.Point(9, 15);
+            this.comboBoxSplitCount.Name = "comboBoxSplitCount";
+            this.comboBoxSplitCount.Size = new System.Drawing.Size(85, 21);
+            this.comboBoxSplitCount.TabIndex = 7;
+            // 
+            // groupBoxTipPercent
+            // 
+            this.groupBoxTipPercent.Controls.Add(this.comboBoxTipPercent);
+            this.groupBoxTipPercent.Location = new System.Drawing.Point(128, 9);
+            this.groupBoxTipPercent.Name = "groupBoxTipPercent";
+            this.groupBoxTipPercent.Size = new System.Drawing.Size(100, 45);
+            this.groupBoxTipPercent.TabIndex = 2;
+            this.groupBoxTipPercent.TabStop = false;
+            this.groupBoxTipPercent.Text = "Tip (%)";
+            this.groupBoxTipPercent.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // comboBoxTipPercent
+            // 
+            this.comboBoxTipPercent.FormattingEnabled = true;
+            this.comboBoxTipPercent.Items.AddRange(new object[] {
+            "10.0",
+            "15.0",
+            "20.0",
+            "25.0",
+            "30.0"});
+            this.comboBoxTipPercent.Location = new System.Drawing.Point(9, 16);
+            this.comboBoxTipPercent.Name = "comboBoxTipPercent";
+            this.comboBoxTipPercent.Size = new System.Drawing.Size(85, 21);
+            this.comboBoxTipPercent.TabIndex = 0;
             // 
             // checkBoxCalculateOnlySubTotal
             // 
             this.checkBoxCalculateOnlySubTotal.AutoSize = true;
             this.checkBoxCalculateOnlySubTotal.Location = new System.Drawing.Point(137, 64);
             this.checkBoxCalculateOnlySubTotal.Name = "checkBoxCalculateOnlySubTotal";
-            this.checkBoxCalculateOnlySubTotal.Size = new System.Drawing.Size(171, 17);
+            this.checkBoxCalculateOnlySubTotal.Size = new System.Drawing.Size(216, 17);
             this.checkBoxCalculateOnlySubTotal.TabIndex = 6;
-            this.checkBoxCalculateOnlySubTotal.Text = "Ignore Tax (Only Use Subtotal)";
+            this.checkBoxCalculateOnlySubTotal.Text = "Ignore Tax (Tip only accounts SubTotal)";
             this.checkBoxCalculateOnlySubTotal.UseVisualStyleBackColor = true;
             // 
             // lblTaxPercent
@@ -87,12 +143,12 @@
             this.lblTaxPercent.TabIndex = 3;
             this.lblTaxPercent.Text = "Tax (%)";
             // 
-            // textBox1
+            // textBoxSubTotalInput
             // 
-            this.textBox1.Location = new System.Drawing.Point(22, 25);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 4;
+            this.textBoxSubTotalInput.Location = new System.Drawing.Point(22, 25);
+            this.textBoxSubTotalInput.Name = "textBoxSubTotalInput";
+            this.textBoxSubTotalInput.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSubTotalInput.TabIndex = 4;
             // 
             // lblSubTotal
             // 
@@ -103,63 +159,98 @@
             this.lblSubTotal.TabIndex = 0;
             this.lblSubTotal.Text = "Sub Total ($)";
             // 
-            // textBox2
+            // textBoxSalesTaxInput
             // 
-            this.textBox2.Location = new System.Drawing.Point(22, 64);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 5;
+            this.textBoxSalesTaxInput.Location = new System.Drawing.Point(22, 64);
+            this.textBoxSalesTaxInput.Name = "textBoxSalesTaxInput";
+            this.textBoxSalesTaxInput.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSalesTaxInput.TabIndex = 5;
             // 
-            // groupBoxTipPercent
+            // calculateButton
             // 
-            this.groupBoxTipPercent.Controls.Add(this.comboBox1);
-            this.groupBoxTipPercent.Location = new System.Drawing.Point(128, 9);
-            this.groupBoxTipPercent.Name = "groupBoxTipPercent";
-            this.groupBoxTipPercent.Size = new System.Drawing.Size(100, 45);
-            this.groupBoxTipPercent.TabIndex = 2;
-            this.groupBoxTipPercent.TabStop = false;
-            this.groupBoxTipPercent.Text = "Tip (%)";
-            this.groupBoxTipPercent.Enter += new System.EventHandler(this.groupBox1_Enter);
+            this.calculateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calculateButton.Location = new System.Drawing.Point(133, 160);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(122, 42);
+            this.calculateButton.TabIndex = 2;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.CalculateButton_Click);
             // 
-            // comboBox1
+            // lblTipTotal
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(9, 16);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(85, 21);
-            this.comboBox1.TabIndex = 0;
+            this.lblTipTotal.AutoSize = true;
+            this.lblTipTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTipTotal.Location = new System.Drawing.Point(12, 221);
+            this.lblTipTotal.Name = "lblTipTotal";
+            this.lblTipTotal.Size = new System.Drawing.Size(237, 31);
+            this.lblTipTotal.TabIndex = 3;
+            this.lblTipTotal.Text = "Tip (Per Person):";
             // 
-            // groupBoxPersonCount
+            // lblSalesTaxTotal
             // 
-            this.groupBoxPersonCount.Controls.Add(this.comboBoxSplitCount);
-            this.groupBoxPersonCount.Location = new System.Drawing.Point(234, 9);
-            this.groupBoxPersonCount.Name = "groupBoxPersonCount";
-            this.groupBoxPersonCount.Size = new System.Drawing.Size(100, 45);
-            this.groupBoxPersonCount.TabIndex = 2;
-            this.groupBoxPersonCount.TabStop = false;
-            this.groupBoxPersonCount.Text = "Split";
+            this.lblSalesTaxTotal.AutoSize = true;
+            this.lblSalesTaxTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSalesTaxTotal.Location = new System.Drawing.Point(97, 279);
+            this.lblSalesTaxTotal.Name = "lblSalesTaxTotal";
+            this.lblSalesTaxTotal.Size = new System.Drawing.Size(152, 31);
+            this.lblSalesTaxTotal.TabIndex = 4;
+            this.lblSalesTaxTotal.Text = "Sales Tax:";
             // 
-            // comboBoxSplitCount
+            // textBoxTipOutput
             // 
-            this.comboBoxSplitCount.FormattingEnabled = true;
-            this.comboBoxSplitCount.Location = new System.Drawing.Point(9, 15);
-            this.comboBoxSplitCount.Name = "comboBoxSplitCount";
-            this.comboBoxSplitCount.Size = new System.Drawing.Size(85, 21);
-            this.comboBoxSplitCount.TabIndex = 7;
+            this.textBoxTipOutput.Location = new System.Drawing.Point(255, 233);
+            this.textBoxTipOutput.Name = "textBoxTipOutput";
+            this.textBoxTipOutput.ReadOnly = true;
+            this.textBoxTipOutput.Size = new System.Drawing.Size(100, 20);
+            this.textBoxTipOutput.TabIndex = 6;
+            // 
+            // textBoxSalesTaxOutput
+            // 
+            this.textBoxSalesTaxOutput.Location = new System.Drawing.Point(255, 291);
+            this.textBoxSalesTaxOutput.Name = "textBoxSalesTaxOutput";
+            this.textBoxSalesTaxOutput.ReadOnly = true;
+            this.textBoxSalesTaxOutput.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSalesTaxOutput.TabIndex = 7;
+            // 
+            // dollarLabel
+            // 
+            this.dollarLabel.AutoSize = true;
+            this.dollarLabel.Location = new System.Drawing.Point(242, 236);
+            this.dollarLabel.Name = "dollarLabel";
+            this.dollarLabel.Size = new System.Drawing.Size(13, 13);
+            this.dollarLabel.TabIndex = 8;
+            this.dollarLabel.Text = "$";
+            // 
+            // dollarLabelTax
+            // 
+            this.dollarLabelTax.AutoSize = true;
+            this.dollarLabelTax.Location = new System.Drawing.Point(242, 294);
+            this.dollarLabelTax.Name = "dollarLabelTax";
+            this.dollarLabelTax.Size = new System.Drawing.Size(13, 13);
+            this.dollarLabelTax.TabIndex = 9;
+            this.dollarLabelTax.Text = "$";
             // 
             // Proj10Form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(384, 361);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dollarLabelTax);
+            this.Controls.Add(this.dollarLabel);
+            this.Controls.Add(this.textBoxSalesTaxOutput);
+            this.Controls.Add(this.textBoxTipOutput);
+            this.Controls.Add(this.lblSalesTaxTotal);
+            this.Controls.Add(this.lblTipTotal);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.tipPanel);
             this.Controls.Add(this.lblTitle);
             this.Name = "Proj10Form";
             this.Text = "CST242 Project 10";
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
-            this.groupBoxTipPercent.ResumeLayout(false);
+            this.tipPanel.ResumeLayout(false);
+            this.tipPanel.PerformLayout();
             this.groupBoxPersonCount.ResumeLayout(false);
+            this.groupBoxTipPercent.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -168,16 +259,23 @@
         #endregion
 
         private System.Windows.Forms.Label lblTitle;
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel tipPanel;
         private System.Windows.Forms.GroupBox groupBoxTipPercent;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBoxTipPercent;
         private System.Windows.Forms.CheckBox checkBoxCalculateOnlySubTotal;
         private System.Windows.Forms.Label lblTaxPercent;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBoxSubTotalInput;
         private System.Windows.Forms.Label lblSubTotal;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBoxSalesTaxInput;
         private System.Windows.Forms.GroupBox groupBoxPersonCount;
         private System.Windows.Forms.ComboBox comboBoxSplitCount;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Label lblTipTotal;
+        private System.Windows.Forms.Label lblSalesTaxTotal;
+        private System.Windows.Forms.TextBox textBoxTipOutput;
+        private System.Windows.Forms.TextBox textBoxSalesTaxOutput;
+        private System.Windows.Forms.Label dollarLabel;
+        private System.Windows.Forms.Label dollarLabelTax;
     }
 }
 
--- Project-10/Form1.cs
+++ Project-10/Form1.cs
@@ -8,18 +8,66 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+/// <summary>
+/// The Namespace for the Project 10 Form.
+/// 
+/// By Andy Osorio, Thomas Lawless, Justin Moran CST242 Spring 2019 SCCC
+/// </summary>
 namespace Project_10
 {
     public partial class Proj10Form : Form
     {
+        /// <summary>
+        /// Declaring the variables to be worked on;
+        /// </summary>
+        private double SubTotal;
+        private double TaxPercent;
+        private double TipSplit;
+        private double TipPercent;
+        private double TipTotal;
+        private double TotalTax;
         public Proj10Form()
         {
             InitializeComponent();
         }
-
+        /// <summary>
+        /// A but method for groupbox 1, removing this breaks the program for some reason
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void groupBox1_Enter(object sender, EventArgs e)
         {
 
+        }
+        /// <summary>
+        /// The event handler for the calculate button. This button takes the input from the above group of controls
+        /// and then initializes the declared variables. It then calculates the Tip based on the input and outputs it 
+        /// to the bottom panel.
+        /// </summary>
+        /// <param name="sender">The Calculate Button</param>
+        /// <param name="e">The Click Event</param>
+        private void CalculateButton_Click(object sender, EventArgs e)
+        {
+            SubTotal = Double.Parse(textBoxSubTotalInput.Text);
+            TaxPercent = Double.Parse(textBoxSalesTaxInput.Text);
+            TipSplit = Double.Parse(comboBoxSplitCount.Text);
+            TipPercent = Double.Parse(comboBoxTipPercent.Text);
+
+            if (checkBoxCalculateOnlySubTotal.Checked == true)
+            {
+                TotalTax = SubTotal * (TaxPercent / 100);
+                textBoxSalesTaxOutput.Text = TotalTax.ToString();
+                TipTotal = SubTotal * (TipPercent / 100);
+                textBoxTipOutput.Text = (TipTotal / TipSplit).ToString();
+            }
+            else
+            {
+                TotalTax = SubTotal * (TaxPercent / 100);
+                textBoxSalesTaxOutput.Text = TotalTax.ToString();
+                SubTotal += TotalTax;
+                TipTotal = SubTotal * (TipPercent / 100);
+                textBoxTipOutput.Text = TipTotal.ToString();
+            }
         }
     }
 }
